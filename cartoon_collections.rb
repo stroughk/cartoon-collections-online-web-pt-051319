8d65ada2935169b6bcd0836e63345f960b556205
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-25 22:38:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,7 @@ def summon_captain_planet(array) # code an argument here
    new_array <<  name.capitalize
     puts "#{name}!"
   end
+  new_array
 end
 
 def long_planeteer_calls# code an argument here
