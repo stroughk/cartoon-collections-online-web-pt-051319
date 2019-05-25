13e485806d4e4c8892e9a28547cc51d6c5ca008f
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-25 22:31:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,13 @@ def roll_call_dwarves(array)    # code an argument here
   end  # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+def summon_captain_planet(array) # code an argument here
+  array.map do |name|
+    puts name.upcase +"!"
+  end 
+  
 end
 
 def long_planeteer_calls# code an argument here
