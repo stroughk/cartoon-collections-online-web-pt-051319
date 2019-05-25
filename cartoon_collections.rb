98314124f511c77cc9349a144abf5f9fb995f9cb
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-25 22:48:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,8 @@
 array = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
 def roll_call_dwarves(array)    # code an argument here
-  array.each_with_index do |array
+  array.each_with_index do |array, index|
+    
 end
 
 
